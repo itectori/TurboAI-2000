510695d41ca5893750eae6ad8e102a6d113fa322
Corentin Mounier <mounie_c@epita.fr>
2019-11-05 18:12:15 +0100

    games: use class to make the code more readable

--- games/connect4/game.py
+++ games/connect4/game.py
@@ -1,31 +1,37 @@
 import numpy as np
 
+class State:
+    def __init__(self, player, grid, last_move):
+        self.player = player
+        self.grid = grid
+        self.last_move = last_move
+
 def init():
-    return np.array([1, np.array([[0] * 6 for _ in range(7)]), -1])
+    return State(1, np.array([[0] * 6 for _ in range(7)]), -1)
 
 def get_all_moves(state):
-    return np.where(state[1].T[0] == 0)[0]
+    return np.where(state.grid.T[0] == 0)[0]
 
 def print_state(state):
     value = np.array(["   ", " X ", " O "])
     print()
     for i in range(6):
-        print(*value[state[1].T[i]], sep="|")
+        print(*value[state.grid.T[i]], sep="|")
     print("|".join(["---"] * 7))
     print(end=" ")
     print(*range(7), sep=" | ")
     print()
 
 def play(state, action):
-    state[1][action][np.where(state[1][action] == 0)[0][-1]] = state[0]
-    state[0] -= 3
-    state[0] *= -1
-    state[2] = action
+    state.grid[action][np.where(state.grid[action] == 0)[0][-1]] = state.player
+    state.player -= 3
+    state.player *= -1
+    state.last_move = action
     return state
 
 def _check(state, x, y, dx, dy):
     c = 0
-    grid = state[1]
+    grid = state.grid
     first = grid[x, y]
     x += dx
     y += dy
@@ -38,11 +44,11 @@ def _check(state, x, y, dx, dy):
     return c
 
 def end(state):
-    if state[2] == -1:
+    if state.last_move == -1:
         return 0
-    last_player = -(state[0] - 3)
-    x = state[2]
-    y = np.where(state[1][x] == last_player)[0][0]
+    last_player = -(state.player - 3)
+    x = state.last_move
+    y = np.where(state.grid[x] == last_player)[0][0]
     if _check(state, x, y, 0, 1) + _check(state, x, y, 0, -1) >= 3:
         return last_player
     if _check(state, x, y, 1, 0) + _check(state, x, y, -1, 0) >= 3:
@@ -56,9 +62,9 @@ def end(state):
     return 0
 
 def encode_input(state):
-    rdn_input = [state[0]]
-    p1 = (state[1].flatten() == 1).astype(np.int)
-    p2 = (state[1].flatten() == 2).astype(np.int)
+    rdn_input = [state.player]
+    p1 = (state.grid.flatten() == 1).astype(np.int)
+    p2 = (state.grid.flatten() == 2).astype(np.int)
     for e in zip(p1, p2):
         rdn_input += e
     return np.array(rdn_input)

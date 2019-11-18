4a0a69d33c03f2a353c1f1349dc404becfe60474
Corentin Mounier <mounie_c@epita.fr>
2019-11-18 12:25:14 +0100

    minmax: implement pv_search

--- MCTS/minimax.py
+++ MCTS/minimax.py
@@ -1,7 +1,7 @@
 import random
 import time
 
-__nb_iter = 20
+__nb_iter = None
 
 def evaluate(game, state):
     score = 0
@@ -21,47 +21,95 @@ def evaluate(game, state):
             score -= 1
     return score / __nb_iter
 
-def minmax(game, state, depth, a = -1, b = 1):
+def pv_search(game, state, depth, pv, pline, a = -2, b = 2):
+    if not pv:
+        return minmax(game, state, depth, pline, a, b)
     end = game.end(state)
     if end:
+        pline[:] = []
         return 0 if end == 3 else -1
     if depth <= 0:
+        pline[:] = []
+        return evaluate(game, state)
+
+    line = []
+    value = -pv_search(game, game.play(state, pv[0]), depth - 1, pv[1:], line, -b, -a)
+    if value >= b:
+        return b
+    if value > a:
+        a = value
+        pline[:] = [pv[0]] + line
+
+    for m in game.get_all_moves(state):
+        if m == pv[0]:
+            continue
+        value = -minmax(game, game.play(state, m), depth - 1, line, -b, -a)
+        if value >= b:
+            return b
+        if value > a:
+            a = value
+            pline[:] = [m] + line
+    return a
+
+
+def minmax(game, state, depth, pline, a = -2, b = 2):
+    end = game.end(state)
+    if end:
+        pline[:] = []
+        return 0 if end == 3 else -1
+    if depth <= 0:
+        pline[:] = []
         return evaluate(game, state)
     value = -1
+    line = []
     for m in game.get_all_moves(state):
-        value = max(value, -minmax(game, game.play(state, m), depth - 1, -b, -a))
-        a = max(a, value)
-        if a >= b:
-            break
-    return value
+        value = -minmax(game, game.play(state, m), depth - 1, line, -b, -a)
+        if value >= b:
+            return b
+        if value > a:
+            a = value
+            pline[:] = [m] + line
+    return a
 
-def play(game, state, max_time=4, nb_iter=30):
-    move = None
-    a = -2
+def log_info(depth, move, a, pourcent):
+    waiting = "#" * (pourcent // 10) + ".........."
+    waiting = waiting[:10]
+    print(f"\rDepth: {depth:>2}", end="")
+    print(f" | Best move: {move}", end="")
+    print(f" | Confidence: {a / 2 + 0.5:.2f}", end="")
+    print(f" | {pourcent:>3}% [{waiting}]    ", end="", flush=True)
+
+
+def play(game, state, max_time=4, nb_iter=20):
+    global __nb_iter
     __nb_iter = nb_iter
+    move = None
     moves = game.get_all_moves(state)
-    waiting = "." * len(moves)
-    print(f"\r[{waiting}] 0%", end="")
-    start_time = time.time()
     depth = 0
+    pv = []
+    start_time = time.time()
     while time.time() < start_time + max_time:
         depth += 1
         a = -2
+        line = []
+        if pv:
+            a = -pv_search(game, game.play(state, move), depth - 1, pv[1:], line, b = -a)
+            pv[:] = [move] + line
         for m in moves:
-            val = -minmax(game, game.play(state, m), depth - 1, b = -a)
+            if pv and m == pv[0]:
+                continue
+            val = -minmax(game, game.play(state, m), depth - 1, line, b = -a)
             if val > a:
                 a = val
                 move = m
+                pv[:] = [m] + line
             approx = 0.8 * (time.time() - start_time) / max_time
-            waiting = "#" * int(len(moves) * approx)
-            waiting += "." * len(moves)
-            waiting = waiting[:len(moves)]
             pourcent = min(100, int(100 * approx))
-            print(f"\r[{waiting}] {pourcent}%", end="")
-        if a == -1 or a == 1 or depth == 100:
+            log_info(depth, move, a, pourcent)
+        if depth >= 99:
             break
-    print(f"\r[{'#' * len(moves)}] 100%")
-    print(f"Confidence: {a/2 + 0.5}")
-    print(f"Depth: {depth}")
+    log_info(depth, move, a, 100)
+    print()
     print(f"Time: {time.time() - start_time:.1f} seconds")
+    print(f"Principal variation: {pv}")
     return move
